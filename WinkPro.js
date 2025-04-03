5d37c30ae9b611143047afb417a8dbf686f27d13
Viết Đạt <vwtdat160@gmail.com>
2025-04-04 00:28:58 +0700

    WinkPro.js

---
+++ WinkPro.js
@@ -0,0 +1,11 @@
+#!name=Wink
+#!desc=Wink解锁会员
+#!icon=https://raw.githubusercontent.com/deezertidal/private/main/icons/.png
+#!homepage=https://whatshub.top
+#!author=yqc007
+[Script]
+WinkForeverVipCrack.js=type=http-response,pattern=^https?:\/\/api-sub\.meitu\.com\/v2\/user\/vip_info_by_group\.json,requires-body=1,script-path=https://raw.githubusercontent.com/yqc007/QuantumultX/master/WinkForeverVipCrack.js
+
+
+[MITM]
+hostname = %APPEND% api-sub.meitu.com
